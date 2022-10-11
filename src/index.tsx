bc33eaceb88a253e51244da6764f4b3c8dc8f591
Ivan Bošković <ivan7vkci7@gmail.com>
2022-10-11 16:22:41 +0200

    Added icons and style filse/folders

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+
 import App from "./App";
+import "./app/styles/App.scss";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
