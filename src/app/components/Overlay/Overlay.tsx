4d7ab9fee5fbfb44cbe13b15f560abfb4ce22863
Ivan Bošković <ivan7vkci7@gmail.com>
2022-10-11 19:20:22 +0200

    Hover overlay slicing

---
+++ src/app/components/Overlay/Overlay.tsx
@@ -0,0 +1,14 @@
+const Overlay = () => {
+  return (
+    <div className="overlay">
+      <div className="overlay__header">
+        <i className="icon icon--lg icon--expand"></i>
+      </div>
+      <div className="overlay__body">
+        <i className="icon icon--lg icon--heart-transparent"></i>
+      </div>
+    </div>
+  );
+};
+
+export default Overlay;
